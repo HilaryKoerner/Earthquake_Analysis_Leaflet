9e50b4122e59580757ccb437fbf2832574e6840b
Hilary <hilary.koerner@gmail.com>
2021-04-18 14:14:33 -0500

    added colors

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,8 @@
 
 // Create a map object.
 var myMap = L.map("map", {
-  center: [15.5994, -28.6731],
-  zoom: 2
+  center: [15.5994, -15.6731],
+  zoom: 3
 });
 
 // Add a tile layer.
@@ -10,31 +10,60 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// function markerSize(population) {
+//   return population / 40;
+// }
+
 
 //earthquake link
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
-//circle color based on size
-var magnitudeSize = {
+function onEachFeature(feature, layer) {
+  var content = (
 
-};
+    "<div>Location:"+ feature.properties.place+"</div>"+"<div>Magnitude:"+ feature.properties.mag+"</div>"+"<div>Depth:"+ feature.geometry.coordinates[2]+"</div>"
+  );
+  layer.bindPopup(content);
+}
+
+function fillColor(depth) {
+  if (depth >50) return "#ff6666";
+  if (depth >75) return "#ff3333";
+  return "989898";
+}
+
+function createMap(mapData) {
+  // L.marker([87,41]).addTo(myMap).bindPopup("Hello").openPopup();
+  L.geoJSON(mapData, 
+    {
+      onEachFeature: onEachFeature,
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(
+          latlng, {
+            radius: +feature.properties.mag*5,
+            fillColor: fillColor(feature.geometry.coordinates[2]),
+            color: "white",
+            weight: 1,
+            opacity: .2,
+            fillOpacity: .8
+          }
+        );
+      }
+    }
+  ).addTo(myMap);
+}
 
 //earthquake data
 d3.json(url)
   .then(function(data) {
     console.log(data);
-
-    var quakes = [];
-
-    data.forEach(function(quake) {
-      var quake = L.circle([quake.coordinates]);
-      quake.addTo(myMap)
-      quakes.push(quake);
+    createMap(data);
     });
-  })
+
+  
 
 
-  bubbles = L.bubbleLayer(data, { property: "mag" })
+  // bubbles = L.bubbleLayer(data, { property: "mag" })
 
 
 //     // Creating a GeoJSON layer with the retrieved data
