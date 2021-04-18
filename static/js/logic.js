9f5cd11568631cb37474a44ff34670323537a02b
Hilary <hilary.koerner@gmail.com>
2021-04-18 09:55:17 -0500

    update code

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 // Create a map object.
 var myMap = L.map("map", {
   center: [15.5994, -28.6731],
-  zoom: 3
+  zoom: 2
 });
 
 // Add a tile layer.
@@ -13,109 +13,56 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //earthquake link
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+
+
+var magnitudeSize = {
+  ''
+}
+
 //earthquake data
-d3.json(url).then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
+d3.json(url)
+  .then(function(data) {
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data, {
+      style: function(feature) {
+        return{
+          color: "white",
+          fillColor: magnitudeSize(feature.properties.mag),
+
+        }
+      }
+      {
+        color: "",
+        size: magnitudeSize(data),
+    }).addTo(myMap);
 });
 
 
-// Loop through the cities array, and create one marker for each city object.
-for (var i = 0; i < countries.length; i++) {
 
-  // Conditionals for country points
-  var color = "";
-  if (countries[i].points > 200) {
-    color = "yellow";
-  }
-  else if (countries[i].points > 100) {
-    color = "blue";
-  }
-  else if (countries[i].points > 90) {
-    color = "green";
-  }
-  else {
-    color = "red";
-  }
+// // Loop through the cities array, and create one marker for each city object.
+// for (var i = 0; i < countries.length; i++) {
 
-  // Add circles to the map.
-  L.circle(countries[i].location, {
-    fillOpacity: 0.75,
-    color: "white",
-    fillColor: color,
-    // Adjust the radius.
-    radius: countries[i].points * 1500
-  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
-}
+//   // Conditionals for country points
+//   var color = "";
+//   if (countries[i].points > 200) {
+//     color = "yellow";
+//   }
+//   else if (countries[i].points > 100) {
+//     color = "blue";
+//   }
+//   else if (countries[i].points > 90) {
+//     color = "green";
+//   }
+//   else {
+//     color = "red";
+//   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// A function to determine the marker size based on the magnitude
-function markerSize(magnitude) {
-  return magnitude / 40;
-}
-
-
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
-
-var myMap = L.map("map", {
-    center: [45.52, 0],
-    zoom: 2
-  });
-  
-// Adding a tile layer (the background map image) to our map:
-// We use the addTo() method to add objects to our map.
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-// Creating the map object
-var myMap = L.map("map", {
-  center: [40.7128, -74.0059],
-  zoom: 11
-});
-
-// Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-// Use this link to get the GeoJSON data.
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
-
-// Getting our GeoJSON data
-d3.json(link).then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
-});
+//   // Add circles to the map.
+//   L.circle(countries[i].location, {
+//     fillOpacity: 0.75,
+//     color: "white",
+//     fillColor: color,
+//     // Adjust the radius.
+//     radius: countries[i].points * 1500
+//   }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+// }
