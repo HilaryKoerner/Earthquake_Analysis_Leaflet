911ee02d45462a60fcaae16d93c5dd2201a5f0ce
Hilary <hilary.koerner@gmail.com>
2021-05-02 17:51:59 -0500

    update colors

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,8 @@
 
 // Create a map object.
 var myMap = L.map("map", {
-    center: [15.5994, -15.6731],
-    zoom: 3
+    center: [41.878113, -87.629799],
+    zoom: 4
   });
   
   // Add a tile layer.
@@ -22,39 +22,17 @@ var myMap = L.map("map", {
     layer.bindPopup(content);
   }
   
-  // function fillColor(depth) {
-  //   if (depth ==77.7) return "red";
-  //   if (depth ==20) return "orange";
-  //   if (depth ==19.34) return "yellow";
-  //   if (depth ==11.61) return "green";
-  //   if (depth ==10) return "purple";
-  //   if (depth ==82.28) return "blue";
-  //   return "teal";
-  // };
-  
-    // Create a new choropleth layer.
-    geojson = L.choropleth(data, {
-  
-      // Define which property in the features to use.
-      valueProperty: "mag",
-  
-      // Set the color scale.
-      scale: ["#ffffb2", "#b10026"],
-  
-      // The number of breaks in the step range
-      steps: 10,
-  
-      // q for quartile, e for equidistant, k for k-means
-      mode: "q",
-      style: {
-        // Border color
-        color: "#fff",
-        weight: 1,
-        fillOpacity: 0.8
-      }
-    })
-  
-  
+  colors = ["#009900", "#cccc00", " #ccff33",  "#00802b", "#ffcc00", "#ff3300"]
+
+  function fillColor(depth) {
+    if (depth <=10) return colors[0];
+    if (depth <=30) return colors[1];
+    if (depth <=50) return colors[2];
+    if (depth <=70) return colors[3];
+    if (depth <=90) return colors[4];
+    return colors[5];
+  };
+
   
   function createMap(mapData) {
     // L.marker([87,41]).addTo(myMap).bindPopup("Hello").openPopup();
@@ -64,8 +42,8 @@ var myMap = L.map("map", {
         pointToLayer: function(feature, latlng) {
           return L.circleMarker(
             latlng, {
-              radius: +feature.properties.mag*5,
-              fillColor: fillColor(feature.geometry.coordinates[2]),
+              radius: +feature.properties.mag*3,
+              fillColor: fillColor(+feature.geometry.coordinates[2]),
               color: "white",
               weight: 1,
               opacity: .2,
@@ -75,7 +53,16 @@ var myMap = L.map("map", {
         }
       }
     ).addTo(myMap);
+    var legend = L.control({
+        position: "bottomright"
+    });
+    legend.onAdd = function createLegend(legend)
+        { 
+        }
+        legend.addTo(myMap);
   }
+
+// on add fcuntion
   
   //earthquake data
   d3.json(url)
@@ -83,50 +70,3 @@ var myMap = L.map("map", {
       console.log(data);
       createMap(data);
       });
-  
-    bubbles = L.bubbleLayer(data, { property: "mag" })
-  
-  
-      // Creating a GeoJSON layer with the retrieved data
-      L.geoJson(data, {
-        style: function(feature) {
-          return{
-            color: "white",
-            fillColor: magnitudeSize(feature.properties.mag),
-          }
-        }
-        {
-          color: "",
-          size: magnitudeSize(data),
-      }).addTo(myMap);
-      }
-    
-      
-  // Loop through the cities array, and create one marker for each city object.
-  for (var i = 0; i < countries.length; i++) {
-  
-    // Conditionals for country points
-    var color = "";
-    if (countries[i].points > 200) {
-      color = "yellow";
-    }
-    else if (countries[i].points > 100) {
-      color = "blue";
-    }
-    else if (countries[i].points > 90) {
-      color = "green";
-    }
-    else {
-      color = "red";
-    }
-  
-    // Add circles to the map.
-    L.circle(countries[i].location, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: color,
-      // Adjust the radius.
-      radius: countries[i].points * 1500
-    }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
-  }
-  
